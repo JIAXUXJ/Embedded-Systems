8902a6c1c0fc102dad96123719cf35bf10fff764
yuhe-jin <yuhejin01@gmail.com>
2016-07-16 17:52:05 -0700

    Add files via upload

--- cordic_TB.c
+++ cordic_TB.c
@@ -8,9 +8,9 @@ int main( void) {
 	x_d = 0.85;
 	y_d = 0.76;
 	z_d = atan( y_d / x_d); /* call to std-C math routine */
-	x_i = x_d * (1 << 15); /* convert to 16-bit integers*/
-	y_i = y_d * (1 << 15); /* convert to 16-bit integers*/
-	z_i = z_d * (1 << 15); /* convert to 16-bit integers*/
+	x_i = x_d * (1 << 14); /* convert to 16-bit integers*/
+	y_i = y_d * (1 << 14); /* convert to 16-bit integers*/
+	z_i = z_d * (1 << 14); /* convert to 16-bit integers*/
 
 	printf( "x_d = %f\t\t\tx_i = %i\n", x_d, x_i);
 	printf( "y_d = %f\t\t\ty_i = %i\n", y_d, y_i);
@@ -18,8 +18,12 @@ int main( void) {
 	printf( "\n");
 	printf( "The angle table:\n");
 	int i;
+	float scale=1;
 	for( i=0; i<15; i++) {
 		printf( "z[%2i] = %i\n", i,
-		(int)(atan( pow( 2.0, (double)(-i))) * (1 << 15)));
+		(int)(atan( pow( 2.0, (double)(-i))) * (1 << 14)));
+		scale=scale*pow(1+pow(2,0-2*i),0.5);
+		printf("scale: %f",scale);
 	}
+	printf("scale: %f",scale);
 }
